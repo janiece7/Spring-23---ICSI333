ad2c94a4a580a36f3dc69f0e607a567a92a2f707
janiece7 <jvpadilla@albany.edu>
2023-02-26 14:21:54 -0500

    first commit

---
+++ pgrm1.c
@@ -0,0 +1,28 @@
+/*
+ICSI 333. System Fundamentals
+Spring 2023
+TA NAME 
+Janiece Mary Padilla 
+001342714
+*/
+
+
+
+
+
+#include <stdio.h>
+int main()
+{
+//Task 1
+// Converting real numbers into binary representation 
+// The whole and fractional parts need different algorithms
+
+printf("Input a real number:");
+
+// Part 1 converting real number to binary ex
+
+// If number is a whole number then
+//convert the base to decimal 
+
+ return 0;
+}
