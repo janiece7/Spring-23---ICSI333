903b8e7a92bf535cc35c79bb6320fd1927079b4a
Janiece7 <64934805+janiece7@users.noreply.github.com>
2023-02-26 16:46:24 -0500

    test 2

--- pgrm1.c
+++ pgrm1.c
@@ -14,14 +14,32 @@ Janiece Mary Padilla
 int main()
 {
 
-    int inputInteger;
+    signed long int inputInteger;
 //Task 1
 // Converting real numbers into binary representation 
 // The whole and fractional parts need different algorithms
 
 // Ask for user Input
 printf("Input a real number:");
-scanf("%d", &inputInteger);
+scanf("%ld", &inputInteger);
+
+/*function declaration*/
+
+void wholenumBin(float inputInteger){
+
+    if (inputInteger > 0 ){
+            /*local variable declaration*/
+            if(inputInteger % 1 > 0){
+                /* Number is a a decimal*/
+                printf("%f is a decimal",inputInteger);
+            }else{
+
+                /*Number is a whole number */
+                 printf("%f is a whole number",inputInteger);
+            }
+    } // End of first if else statement 
+
+} // End of functiom
 
 
 
